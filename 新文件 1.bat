d991818c4e5ccf89fbb03c58ea9f8215a246798b
Dai-97 <2900866932@qq.com>
2024-06-26 17:24:40 +0800

    自动定时执行文件至github

--- 新文件 1.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set PATH=%PATH%;D:\Git\bin
-cd D:\demo
-set COMMIT_MESSAGE="自动定时执行文件至github"
-git add .
-git commit -m %COMMIT_MESSAGE%
-git push origin master
--- 自动同步脚本.bat
+++ 自动同步脚本.bat
@@ -1,6 +1,7 @@
 @echo off
 set PATH=%PATH%;D:\Git\bin
 cd D:\demo
+chcp 65001 >nul
 set COMMIT_MESSAGE="自动定时执行文件至github"
 git add .
 git commit -m %COMMIT_MESSAGE%
